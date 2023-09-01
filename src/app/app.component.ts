7785bd8a3683e463e8afcc1c8de8021ed4d1b86b
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2023-09-01 16:25:04 +0300

    removed semicolons and some comms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,41 +37,36 @@ export class AppComponent implements OnInit {
   }
 
   addVehicle(): void {
-    const entryTimeDate = new Date(this.entryTimeInput); // Convert the input string to a Date object
+    const entryTimeDate = new Date(this.entryTimeInput) // Convert the input string to a Date object
   
     if (isNaN(entryTimeDate.getTime())) {
-      // Set the message for an invalid date
-      this.message = 'Please enter a valid entry time.';
-      // Show the message
-      this.showMessage = true;
-      return;
+      this.message = 'Please enter a valid entry time.'
+      this.showMessage = true
+      return
     }
   
     if (!this.license || !entryTimeDate) {
-      // Set the message
-      this.message = 'Please enter both the license plate and entry time.';
-      // Show the message
-      this.showMessage = true;
-      return;
+      this.message = 'Please enter both the license plate and entry time.'
+      this.showMessage = true
+      return
     }
   
-    const isAdded = this.carService.parkVehicle(this.license, entryTimeDate);
+    const isAdded = this.carService.parkVehicle(this.license, entryTimeDate)
   
     if (isAdded) {
       // Update available parking slots
-      this.availableParkingSlots = this.carService.getAvailableParkingSpaces();
+      this.availableParkingSlots = this.carService.getAvailableParkingSpaces()
   
       // Clear the input fields after parking
-      this.license = '';
-      this.entryTimeInput = ''; // Reset the entry time input
+      this.license = ''
+      this.entryTimeInput = ''
   
       // Update the car lists
-      this.updateCarLists();
+      this.updateCarLists()
     } else {
       // Handle the case where the vehicle couldn't be added (e.g., parking full)
-      // You can show an error message here if needed
-      this.message = 'Parking is full. Cannot add more vehicles.';
-      this.showMessage = true;
+      this.message = 'Parking is full. Cannot add more vehicles.'
+      this.showMessage = true
     }
   }
   
@@ -79,24 +74,23 @@ export class AppComponent implements OnInit {
 
   
   exitVehicle(licensePlate: string): void {
-    const exitedVehicle = this.carService.exitVehicle(licensePlate);
+    const exitedVehicle = this.carService.exitVehicle(licensePlate)
     if (exitedVehicle) {
       // Update the vehicle list with exit information
-      const index = this.parkedVehicles.findIndex((v) => v.licensePlate === exitedVehicle.licensePlate);
+      const index = this.parkedVehicles.findIndex((v) => v.licensePlate === exitedVehicle.licensePlate)
       if (index !== -1) {
-        this.parkedVehicles[index] = exitedVehicle;
+        this.parkedVehicles[index] = exitedVehicle
       }
     }
     // Update available parking slots
-    this.updateAvailableParkingSlots();
-    this.updateCarLists();
+    this.updateAvailableParkingSlots()
+    this.updateCarLists()
 
-    this.cdr.detectChanges(); // Ensure change detection is triggered
+    this.cdr.detectChanges() // Ensure change detection is triggered
   }
 
   closeMessage(): void {
-    // Close the message
-    this.showMessage = false;
-    this.message = '';
+    this.showMessage = false
+    this.message = ''
   }
 }
--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -12,12 +12,12 @@ export class CarService {
 
   getAvailableParkingSpaces(): number {
     // Calculate the number of parked vehicles (those with isParked set to true)
-    const parkedVehiclesCount = this.parkingSlots.filter((car) => car.isParked).length;
+    const parkedVehiclesCount = this.parkingSlots.filter((car) => car.isParked).length
     
     // Calculate the available parking spaces
-    const availableSpaces = this.parkingCapacity - parkedVehiclesCount;
+    const availableSpaces = this.parkingCapacity - parkedVehiclesCount
   
-    return availableSpaces;
+    return availableSpaces
   }
   
 
@@ -27,7 +27,7 @@ export class CarService {
 
   parkVehicle(licensePlate: string, entryTime: Date): boolean {
     if (!licensePlate || !entryTime) {
-      return false; // Validation failed
+      return false
     }
   
     if (this.parkingSlots.length < this.parkingCapacity) {
@@ -38,7 +38,7 @@ export class CarService {
         isParked: true
       };
       this.parkingSlots.push(vehicle)
-      return true // Vehicle added successfully
+      return true 
     }
   
     return false // Parking lot is full
@@ -46,34 +46,34 @@ export class CarService {
   
 
   exitVehicle(licensePlate: string): Car | null {
-    const exitedVehicleIndex = this.parkingSlots.findIndex((car) => car.licensePlate === licensePlate);
+    const exitedVehicleIndex = this.parkingSlots.findIndex((car) => car.licensePlate === licensePlate)
   
     if (exitedVehicleIndex !== -1) {
-      const exitedVehicle = this.parkingSlots[exitedVehicleIndex];
+      const exitedVehicle = this.parkingSlots[exitedVehicleIndex]
       
       if (!exitedVehicle.isParked) {
         // The car has already exited, do nothing
-        return null;
+        return null
       }
   
       const exitTime = new Date(); // Get the current time as the exit time
-      exitedVehicle.exitTime = exitTime;
+      exitedVehicle.exitTime = exitTime
   
       // Calculate the parking duration in milliseconds
-      const entryTime = exitedVehicle.entryTime;
-      const durationMilliseconds = exitTime.getTime() - entryTime.getTime();
+      const entryTime = exitedVehicle.entryTime
+      const durationMilliseconds = exitTime.getTime() - entryTime.getTime()
   
       // Calculate the parking duration in hours (rounded up)
-      const durationHours = Math.ceil(durationMilliseconds / (1000 * 60 * 60));
+      const durationHours = Math.ceil(durationMilliseconds / (1000 * 60 * 60))
   
       // Calculate the parking fee based on the given rates
-      let parkingFee = 10; // Initial fee for the first hour
+      let parkingFee = 10 // Initial fee for the first hour
       if (durationHours > 1) {
-        parkingFee += (durationHours - 1) * 5; // Additional fee for each hour
+        parkingFee += (durationHours - 1) * 5 // Additional fee for each hour
       }
   
       exitedVehicle.parkingFee = parkingFee;
-      exitedVehicle.isParked = false; // Set isParked to false for exited cars
+      exitedVehicle.isParked = false // Set isParked to false for exited cars
      
       return exitedVehicle; // Return the exited vehicle with updated information
     }
